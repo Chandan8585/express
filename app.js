2d00b6fc02fed351c5010e9f063093e70072aeb9
Chandan8585 <chandanwingshr@gmail.com>
2023-08-05 00:57:12 +0530

    api fetching

--- app.js
+++ app.js
@@ -1,16 +1,19 @@
-const express = require("express");
-const app = express();
-const port = process.env.PORT || 3001;
 
-const quizzes = require("./db/quizzes")
+const express = require("express");
+const quizzes = require("./db/quizzes");
+const app = express();
+const port = 8080;
+
+
+
 app.get("/", (req, res) => res.send("Hello Geeks"));
 
 app.get("/quiz", (req, res)=> {
-    res.send("quizzes")
+    res.send(quizzes)
 })
 
 // const server =
- app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+ app.listen(port, () => {console.log(`Example app listening on port ${port}!`)});
 
 // server.keepAliveTimeout = 120 * 1000;
 // server.headersTimeout = 120 * 1000;
