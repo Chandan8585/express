9f648b89b1229e89e4693f3c21b7fbb17e2e3832
Chandan8585 <chandanwingshr@gmail.com>
2023-08-05 00:11:49 +0530

    checking quizzes

--- app.js
+++ app.js
@@ -2,7 +2,12 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3001;
 
+const quizzes = require("./db/quizzes")
 app.get("/", (req, res) => res.send("Hello Geeks"));
+
+app.get("/quiz", (req, res)=> {
+    res.send("quizzes")
+})
 
 // const server =
  app.listen(port, () => console.log(`Example app listening on port ${port}!`));
